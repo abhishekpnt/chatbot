6c01d05d6959479e001e733c86fd358776293130
princegupta1131 <114015020+princegupta1131@users.noreply.github.com>
2024-02-29 17:03:28 +0530

    fix multiple service call post logout

--- src/app/src/components/bot/bot.component.ts
+++ src/app/src/components/bot/bot.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, EventEmitter, Input, NgZone, OnInit, Output, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, EventEmitter, Input, NgZone, OnDestroy, OnInit, Output, ViewChild } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 import { BotMessage } from 'src/app/appConstants';
 import { AudioRecordingService } from 'src/app/src/services/audio-recording.service';
@@ -6,7 +6,6 @@ import { BotApiService } from '../../services/bot-api.service';
 import { LoginService } from '../../services/login.service';
 import { TranslateService } from '@ngx-translate/core';
 import { UtilService } from '../../services/util.service';
-import { Observable } from 'rxjs';
 
 
 @Component({
@@ -14,7 +13,7 @@ import { Observable } from 'rxjs';
   templateUrl: './bot.component.html',
   styleUrls: ['./bot.component.scss'],
 })
-export class BotComponent implements OnInit, AfterViewInit {
+export class BotComponent implements OnInit, AfterViewInit, OnDestroy {
   botMessages: Array<any> = [];
   textMessage: string = ''
   chat!: BotMessage;
@@ -35,7 +34,8 @@ export class BotComponent implements OnInit, AfterViewInit {
   @Output() botMessageEvent = new EventEmitter();
   @ViewChild('recordbtn', { read: ElementRef }) recordbtn: ElementRef | any;
   @ViewChild('content', { static: false }) content: ElementRef;
-  private recordedBlob$: Observable<any>;
+  // private recordedBlob$: Observable<any>;
+  private recordedBlob$: Subscription;
 
   constructor(
     private audioRecordingService: AudioRecordingService, private botApiService: BotApiService,
@@ -55,8 +55,7 @@ export class BotComponent implements OnInit, AfterViewInit {
     this.audioRecordingService.recordingFailed().subscribe(() => (this.isRecording = false));
     this.audioRecordingService.getRecordedTime().subscribe((time) => (this.recordedTime = time));
     if (!this.recordedBlob$) {
-      this.recordedBlob$ = this.audioRecordingService.getRecordedBlob();
-      this.recordedBlob$.subscribe(async (data) => {
+      this.recordedBlob$ = this.audioRecordingService.getRecordedBlob().subscribe(async (data) => {
         this.data = data;
         this.blobUrl = this.sanitizer.bypassSecurityTrustUrl(
           URL.createObjectURL(data.blob)
@@ -87,60 +86,61 @@ export class BotComponent implements OnInit, AfterViewInit {
   async makeBotAPICall(text: string, audio: string) {
     this.textMessage = "";
     this.disabled = true;
-
-    try {
-      const lang = await this.utils.getLanguage() || 'en';
-      text = localStorage.getItem('text') ?? '';
-
-      const result = await this.botApiService.getBotMessage(text, audio, lang).toPromise();
-      console.log('-----', result);
-
-      this.botMessages = [...this.botMessages]; // Clone the array to trigger change detection
-
-      if (result.conversation && result.content) {
-        const data = result;
-        this.utils.setItem('content_id', data?.content?.content_id || '');
-        this.utils.setItem('text', data?.content?.text || '');
-
-        this.botMessages.pop();
-        this.setBotResponse(data?.conversation?.audio, data.conversation?.text);
-        setTimeout(() => {
-          this.setBotResponse(data?.content?.audio, data.content?.text);
+    // Api call and response from bot, replace loading text
+    let index = this.botMessages.length;
+    let lang = await this.utils.getLanguage() || 'en'
+    text = localStorage.getItem('text') ?? '';
+    await this.botApiService.getBotMessage(text, audio, lang).subscribe({
+      next: result => {
+        console.log('-----', result)
+        this.botMessages = JSON.parse(JSON.stringify(this.botMessages));
+        if (result.conversation && result.content) {
+          this.utils.setItem('content_id', result?.content?.content_id || '');
+          this.utils.setItem('text', result?.content?.text || '');
+          this.botMessages.pop();
+          this.setBotResponse(result?.conversation?.audio, result?.conversation?.text);
+          setTimeout(() => {
+            this.setBotResponse(result?.content?.audio, result.content?.text);
+            setTimeout(() => {
+              this.scrollToBottom();
+            }, 100); // Adjust this delay as needed
+          }, 5000);
+        } else {
+          // let textMsg = { identifier: "", message: '', messageType: '', displayMsg: "", audio: { file: '', duration: '', play: false }, type: 'received', time: new Date().toLocaleTimeString('en', { hour: '2-digit', minute: '2-digit' }), timeStamp: Date.now(), readMore: false, likeMsg: false, dislikeMsg: false, requestId: "" }
+          // textMsg.message = data?.output?.text
+          // textMsg.messageType = 'text';
+          // textMsg.displayMsg = data.output?.text
+          // console.log('text recieved', textMsg)
+          // this.botMessages.pop();
+          // this.botMessages.push(textMsg);
+          // console.log('array', this.botMessages)
+          if (result.conversation) {
+            this.botMessages.pop();
+            this.setBotResponse(result?.conversation?.audio, result?.conversation?.text);
+          }
+          if (result.content) {
+            this.botMessages.pop();
+            this.setBotResponse(result?.content?.audio, result.content?.text);
+          }
+          this.scrollToBottom();
           setTimeout(() => {
             this.scrollToBottom();
-          }, 100); // Adjust this delay as needed
-        }, 5000);
-      } else {
-        if (result.conversation) {
-          this.botMessages.pop();
-          this.setBotResponse(result.conversation.audio, result.conversation.text);
+          }, 500);
         }
-        if (result.content) {
-          this.botMessages.pop();
-          this.setBotResponse(result.content.audio, result.content.text);
-        }
+      },
+      error: e => {
+        this.disabled = false;
+        console.log('catch error ', e);
+        let errorMsg = { identifier: "", message: 'errorMessage', messageType: 'text', type: 'received', displayMsg: "errorMessage", time: new Date().toLocaleTimeString('en', { hour: '2-digit', minute: '2-digit' }), timeStamp: '', readMore: false, likeMsg: false, dislikeMsg: false, requestId: "" };
+        this.botMessages.pop();
+        this.botMessages.push(errorMsg);
         this.scrollToBottom();
         setTimeout(() => {
           this.scrollToBottom();
-        }, 500);
+        }, 500)
       }
-    } catch (error) {
-      this.handleError(error);
-    }
+    });
   }
-
-  private handleError(error: any) {
-    this.disabled = false;
-    console.log('catch error ', error);
-    const errorMsg = { identifier: "", message: 'errorMessage', messageType: 'text', type: 'received', displayMsg: "errorMessage", time: new Date().toLocaleTimeString('en', { hour: '2-digit', minute: '2-digit' }), timeStamp: '', readMore: false, likeMsg: false, dislikeMsg: false, requestId: "" };
-    this.botMessages.pop();
-    this.botMessages.push(errorMsg);
-    this.scrollToBottom();
-    setTimeout(() => {
-      this.scrollToBottom();
-    }, 500);
-  }
-
   setBotResponse(audio, text) {
     let audioMsg = { identifier: "", message: '', messageType: '', displayMsg: "", audio: { file: '', duration: '', play: false }, type: 'received', time: new Date().toLocaleTimeString('en', { hour: '2-digit', minute: '2-digit' }), timeStamp: Date.now(), readMore: false, likeMsg: false, dislikeMsg: false, requestId: "" }
     audioMsg.audio = { file: audio, duration: '10', play: false }
@@ -176,6 +176,9 @@ export class BotComponent implements OnInit, AfterViewInit {
 
   ngOnDestroy(): void {
     this.abortRecording();
+    if (this.recordedBlob$) {
+      this.recordedBlob$.unsubscribe(); // Unsubscribe to prevent memory leaks
+    }
   }
 
   playAudio(index) {
@@ -210,6 +213,9 @@ export class BotComponent implements OnInit, AfterViewInit {
   logout() {
     this.loginService.logout();
     localStorage.clear();
+    if (this.recordedBlob$) {
+      this.recordedBlob$.unsubscribe(); // Unsubscribe to prevent memory leaks
+    }
   }
 
 
